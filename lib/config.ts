c6a85716a39870f1042479028cc3692e6e41277b
Jakob Deutsch <jakob.d@coverdash.com>
2025-02-19 12:14:00 -0500

    Update Braintree config to dynamically generate redirect URI based on environment

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,11 @@
+const origin =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
+    ? `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+
 export const BRAINTREE_CONFIG = {
   merchantId: process.env.NEXT_PUBLIC_BRAINTREE_MERCHANT_ID,
   clientId: `client_id$sandbox$${process.env.NEXT_PUBLIC_BRAINTREE_OAUTH_CLIENT_ID}`,
-  redirectUri: process.env.NEXT_PUBLIC_BRAINTREE_REDIRECT_URI,
+  redirectUri: `${origin}${process.env.NEXT_PUBLIC_BRAINTREE_REDIRECT_URI}`,
   environment: process.env.NEXT_PUBLIC_BRAINTREE_ENVIRONMENT,
 };
